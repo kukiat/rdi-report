90362f54c4ad38416a3d1bc2c24be7dd2f23a9c8
kukiat <kukiat1996@gmail.com>
2019-04-07 03:46:03 +0700

    Fix param path

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   return result.data.allPartnersListJson.edges.forEach(({ node }) => {
     actions.createPage({
-      path: `/partners/${node.id}`,
+      path: `/partners/${node.path}`,
       component: pathFile.resolve('./src/components/partner/index.js'),
       context: {
         id: 'efb723cf-62ed-588d-958e-8552181a98bc',
