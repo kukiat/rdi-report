e60480655cfc8a822dcf133af2d9a169e15d7da8
Kao Udomsub Naikorn <udomsub.nk@gmail.com>
2019-04-11 20:51:15 +0700

    :art: use blank div instead wrong graph

--- src/components/Graph10/index.js
+++ src/components/Graph10/index.js
@@ -19,7 +19,7 @@ export default () => {
           <div style={{
             width: '100%',
             height: '600px',
-            backgroundColor: '#aa3434',
+            backgroundColor: '#8c63b1',
           }} />
 
         </div>
