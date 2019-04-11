6d3d5eeb1cc210e575e635e504f9eb3bc47b5a76
Kao Udomsub Naikorn <udomsub.nk@gmail.com>
2019-04-11 20:37:07 +0700

    :art: separated style for Graph9

--- src/components/Graph9/index.js
+++ src/components/Graph9/index.js
@@ -222,23 +222,23 @@ const plugins = {
 
 export default () => {
   return (
-    <div className="graph8-graph-page">
+    <div className="graph9-graph-page">
       <div className="graph-header graph-content" style={{ marginTop: 50 }}>
         <div data-aos="fade-up">
           <p className="graph-title">
             ค่าใช้จ่ายด้านการวิจัยและพัฒนา
             </p>
-          <p className="graph8-graph-subtitle">
+          <p className="graph9-graph-subtitle">
             จำแนกตามประเภทของค่าใช้จ่ายด้านการวิจัยและพัฒนา ปี 2560
             </p>
         </div>
       </div>
       <div className="row">
-        <div className="col-lg-4 graph8-content">
-          <p className="graph8-content-text" data-aos="fade-right">
+        <div className="col-lg-4 graph9-content">
+          <p className="graph9-content-text" data-aos="fade-right">
             โดยค่าใช้จ่ายส่วนใหญ่ร้อยละ 54.45 เป็นค่าใช้จ่ายสำหรับบุคลากรด้านการวิจัยและพัฒนา
             </p>
-          <p className="graph8-content-text" data-aos="fade-right">
+          <p className="graph9-content-text" data-aos="fade-right">
             รองลงมาคือค่าใช้จ่ายสำหรับเครื่องจักรและอุปกรณ์ ร้อยละ 19.26 และค่าใช้จ่ายอื่นๆ ร้อยละ 16.25
             </p>
         </div>
