5013999859b51758349afe9316ba8f523c887457
kukiat <kukiat1996@gmail.com>
2019-04-13 18:17:50 +0700

    Add empty list

--- src/components/PartnersList/index.js
+++ src/components/PartnersList/index.js
@@ -13,16 +13,24 @@ const partnerTypeMapper = (type) => {
 
 const PartnersList = ({ partners }) => {
   return (
-    partners.map((partner, index) => (
-      <Link className="row aabbaa" key={uuid()} to={`/partners/${partner.partnerId}`}>
-        <div className="col-6 animated-fade" css={{ padding: "13px", backgroundColor: index % 2 && "#f8f8f8" }}>
-          {partner.name}
-        </div>
-        <div className="col-6 animated-fade" css={{ padding: "13px", backgroundColor: index % 2 && "#f8f8f8" }}>
-          {partnerTypeMapper(partner.type)}
-        </div>
-      </Link>
-    ))
+    partners.length === 0 ? (
+      <div css={{
+        fontSize: '24px',
+        textAlign: 'center',
+        padding: '10px'
+      }}>ไม่มีลิสต์ที่เลือก</div>
+    ) : (
+        partners.map((partner, index) => (
+          <Link className="row aabbaa" key={index} to={`/partners/${partner.partnerId}`}>
+            <div className="col-6 animated-fade" css={{ padding: "13px", backgroundColor: index % 2 && "#f8f8f8" }}>
+              {partner.name}
+            </div>
+            <div className="col-6 animated-fade" css={{ padding: "13px", backgroundColor: index % 2 && "#f8f8f8" }}>
+              {partnerTypeMapper(partner.type)}
+            </div>
+          </Link>
+        ))
+      )
   )
 }
 
