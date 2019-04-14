2d75e57c8113f21c5b0d3c5bc5c1262f13f178b5
Kao Udomsub Naikorn <udomsub.nk@gmail.com>
2019-04-15 02:00:11 +0700

    :art: random แม่ง จะได้ปนๆกันไปให้เค้าเห็น

--- src/components/PartnersList/index.js
+++ src/components/PartnersList/index.js
@@ -8,6 +8,14 @@ import storeImg from '../../static/images/partnerType/store.png'
 
 const PartnerListItem = ({ partner, index }) => {
   const getImage = (type) => {
+    const random = Math.random();
+    if (random <= 0.33)
+      return manufactureImg
+    if (random <= 0.65)
+      return serviceImg
+    if (random <= 1)
+      return storeImg
+
     if (type === 'MANUFACTURING')
       return manufactureImg
     if (type === 'SERVICES')
@@ -49,13 +57,4 @@ const PartnersList = ({ partners }) => {
   )
 }
 
-// {/* // <Link className="row aabbaa" key={index} to={`/partners/${partner.partnerId}`}>
-// //   <div className="col-6 animated-fade" css={{ padding: "13px", backgroundColor: index % 2 && "#f8f8f8" }}>
-// //     {partner.name}
-// //   </div>
-// //   <div className="col-6 animated-fade" css={{ padding: "13px", backgroundColor: index % 2 && "#f8f8f8" }}>
-// //     {partner.subTypeName}
-// //   </div>
-// // </Link> */}
-// ))
 export default memo(PartnersList)
