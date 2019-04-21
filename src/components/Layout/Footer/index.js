fc07a5a98f20871165fc99cd4f28a5c2faa73ef9
Kao Udomsub Naikorn <udomsub.nk@gmail.com>
2019-04-21 23:27:19 +0700

    :art: Add Footer Content

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "gatsby"
 import Styled from 'styled-components'
 import './index.css'
 
@@ -26,63 +27,56 @@ const FooterBottom = Styled.div`
   font-size: 13px;
 `;
 
-const Link = Styled.a`
-  color: #fff !important;
-`;
-
 export default () => (
   <Footer className='footer'>
     <FooterTop className="footer-top">
       <div className="container">
         <div className="row">
-          <div className="col-lg-1"></div>
-          <div className="col-lg-5">
-            <h6>ABOUT IRIS</h6>
-            <div style={{ marginTop: '25px' }}>Contrary to popular belief, Lorem Ipsum is not.</div>
-            <div style={{ marginTop: '5px' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the Contrary to popular belief, Lorem Ipsum is not a random text.</div>
-          </div>
-          <div className="col-lg-1"></div>
-          <div className="col-lg-2">
-            <div>
-              <h6>SOLUTIONS</h6>
+          <div className="col-lg-10 offset-lg-1 row">
+            <div className="col-sm-8">
+              <h6>PARTICIPATOR</h6>
               <div style={{ marginTop: '25px' }}>
-                <div style={{ paddingLeft: '10px' }}>Contact & Temp</div>
-                <div style={{ paddingLeft: '10px', marginTop: 13 }}>Perm</div>
-                <div style={{ paddingLeft: '10px', marginTop: 13 }}>Exec Search</div>
+                สำนักงานคณะกรรมการนโยบายวิทยาศาสตร์ เทคโนโลยีและนวัตกรรมแห่งชาติ (สวทน.)
+              </div>
+              <div>
+                กระทรวงวิทยาศาสตร์และเทคโนโลยี
+              </div>
+              <div>
+                <a className="footer-link" target="_blank" href="http://www.sti.or.th">http://www.sti.or.th</a>
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                IRIS Consulting Company Limited
+              </div>
+              <div>
+                <a className="footer-link" target="_blank" href="https://www.irisconsulting.co.th">https://www.irisconsulting.co.th</a>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div>
+                <h6>RESOURCES</h6>
+                <div style={{ marginTop: '25px' }}>
+                  <div style={{ paddingLeft: '10px' }}>
+                    <Link className="footer-link" to="/">Home</Link>
+                  </div>
+                  <div style={{ paddingLeft: '10px', marginTop: 13 }}>
+                    <Link className="footer-link" to="/partners">R&D Company</Link>
+                  </div>
+                  <div style={{ paddingLeft: '10px', marginTop: 13 }}>
+                    <Link className="footer-link" to="/reports">Download</Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-lg-2">
-            <div>
-              <h6>RESOURCES</h6>
-              <div style={{ marginTop: '25px' }}>
-                <div style={{ paddingLeft: '10px' }}>About</div>
-                <div style={{ paddingLeft: '10px', marginTop: 13 }}>Careers</div>
-                <div style={{ paddingLeft: '10px', marginTop: 13 }}>Webinars</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-1"></div>
         </div>
       </div>
     </FooterTop>
     <FooterBottom className="footer-bottom">
       <div className="container">
         <div>
-          <Link href="#"> Terms of Service </Link>
-          &nbsp; | &nbsp;
-          <Link href="#">Privacy Policy</Link>
-          &nbsp; | &nbsp;
-          <Link href="#">Act on Specified Commercial Transactions</Link>
-        </div>
-        <div>
-          <span>
-            <Link href="#">Data&nbsp; </Link>
-            <Link href="#">Processing&nbsp; </Link>
-            <Link href="#">Agreement</Link>
-            &nbsp; | &nbsp;
-            <Link href="#">Status</Link>
-          </span>
+          <a className="footer-link" target="_blank" href="http://www.sti.or.th">National Science Technology and Innovation Policy Office</a>
+          {' | '}
+          <a className="footer-link" target="_blank" href="https://www.irisconsulting.co.th">IRIS Consulting Company Limited</a>
         </div>
       </div>
     </FooterBottom>
