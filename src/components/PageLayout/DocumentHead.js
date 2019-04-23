954f667999f1d09143a1c2611625e0bdc0ffe3bf
Kao Udomsub Naikorn <udomsub.nk@gmail.com>
2019-04-24 00:57:31 +0700

    :art: add responsive meta tag

--- src/components/PageLayout/DocumentHead.js
+++ src/components/PageLayout/DocumentHead.js
@@ -73,6 +73,7 @@ const DocumentHead = ({ description, lang, meta, keywords, title }) => {
         )
         .concat(meta)}
     >
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <link
         rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
