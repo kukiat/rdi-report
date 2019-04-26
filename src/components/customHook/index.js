ae2a4d768888bfcaef4607eb4b8d11d95f2e8eb6
kukiat <kukiat1996@gmail.com>
2019-04-27 01:36:18 +0700

    Set useCallback

--- src/components/customHook/index.js
+++ src/components/customHook/index.js
@@ -1,53 +1,50 @@
-import { useRef, useEffect, useState, useContext } from 'react'
+import { useRef, useEffect, useState, useContext, useCallback } from 'react'
 import ScreenContext from '../HumanGraph/ScreenContext'
 
 export const useScreenSize = (initialValue = 0) => {
   const [width, setwidth] = useState(initialValue)
-
-  const onResizeScreen = () => {
+  const onResizeScreen = useCallback(() => {
     setwidth(window.innerWidth)
-  }
+  }, [])
 
   useEffect(() => {
     onResizeScreen()
     window.addEventListener('resize', onResizeScreen, true)
     return () => window.removeEventListener('resize', onResizeScreen, true)
-  }, [width])
+  }, [])
 
   return { width }
 }
 
 export const useRefScreen = () => {
-  const humanGraphRef = useRef()
+  const ref = useRef()
   const [offsetTop, setOffsetTop] = useState(0)
 
   useEffect(() => {
     setOffsetTop(humanGraphRef.current.offsetTop)
   }, [offsetTop])
 
-  return {
-    offsetTop,
-    ref: humanGraphRef
-  }
+  return { offsetTop, ref }
 }
 
 const useOnScrollCheckpoint = () => {
   const { offsetTop } = useContext(ScreenContext)
   const [checkpoint, setCheckpoint] = useState(false)
 
-  const onScroll = () => {
-    if (window.scrollY > 2700) {
+  const onScroll = useCallback(() => {
+    if (window.scrollY > 2600) {
       return setCheckpoint(true)
     }
     return setCheckpoint(false)
-  }
+  }, [])
 
   useEffect(() => {
+    onScroll()
     window.addEventListener('scroll', onScroll)
     return () => {
       window.removeEventListener('scroll', onScroll)
     };
-  }, [offsetTop])
+  }, [])
 
   return checkpoint
 }
